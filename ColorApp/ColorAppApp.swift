8c55c170fbce1e97c6647107eb1c76c163a9a2d9
AlenaBelenets <alenaharenkova@gmail.com>
2022-09-13 16:44:21 +0300

    Initial Commit

---
+++ ColorApp/ColorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorAppApp.swift
+//  ColorApp
+//
+//  Created by Alena Belenets on 13.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
